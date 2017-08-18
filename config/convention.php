fb89ee61e373866aaab0faa9b8aedfe23b1df583
jason9596 <2425399474@qq.com>
2017-08-18 18:57:26 +0800

    add files

---
+++ config/convention.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ *框架的基本配置文件
+ */
+return array(
+	/*数据库配置*/
+	'DB_TYPE' =>'',
+	'DB_HOST' =>'localhost',
+	'DB_NAME' =>'test',
+	'DB_USER' =>'root',
+	'DB_PWD'  =>'root',
+	'DB_CHARSET' =>'utf-8',
+	/*SESSION设置*/
+	'SESSION_AUTO_START' => true,
+);
